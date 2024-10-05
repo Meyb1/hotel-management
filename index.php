83a9ec8eff95646f52088af417c5410d74ca300e
Nyx <61583597+mrlittleman@users.noreply.github.com>
2024-10-05 11:39:18 +0000

    "remove folders structure"

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FINANCE PROTOTYPE</title>
-    <?php include 'partials/header.php'; ?>
+    <?php include 'header.php'; ?>
 </head>
 <body class="light-mode">
-    <script src="js/index.js"></script>
+    <script src="index.js"></script>
 </body>
 </html>
--- job_postings.php
+++ job_postings.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FINANCE PROTOTYPE</title>
-    <?php include 'partials/header.php'; ?>
+    <?php include 'header.php'; ?>
 </head>
 <div id="job-postings"></div>
-<form action="php/job_operations.php" method="POST" onsubmit="event.preventDefault(); addJob();">
+<form action="job_operations.php" method="POST" onsubmit="event.preventDefault(); addJob();">
     <input type="text" name="job_title" placeholder="Job Title" required>
     <textarea name="job_description" placeholder="Job Description" required></textarea>
     <button type="submit">Add Job</button>
 </form>
 
-<script src="js/index.js"></script>
-<script src="js/job_postings.js"></script>
+<script src="index.js"></script>
+<script src="job_postings.js"></script>
