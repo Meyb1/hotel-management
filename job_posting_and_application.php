0b53ef18f6c0da413f96e79723420deae078bb53
Nyx <61583597+mrlittleman@users.noreply.github.com>
2024-10-05 17:02:33 +0000

    "fixes"

--- job_posting_and_application.php
+++ job_posting_and_application.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Posting Application</title>
     <link rel="stylesheet" href="index.css">
-    <?php include 'header.php';?>
+    <?php include 'header.php'?>
 </head>
 <body class="light-mode">
     <div class="job-posting-container">
