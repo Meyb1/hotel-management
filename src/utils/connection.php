f3c2c9bdbcc08b6e5de2163d0da9726cf70a6047
Meyb1 <maiveveloso0@gmail.com>
2024-10-05 13:00:45 +0800

    updates

---
+++ src/utils/connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$databasename = 'hotelrestaurantmanagement';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
