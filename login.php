f96dd17524f49ba6bf323c4ff83400ecf339126d
Nyx <61583597+mrlittleman@users.noreply.github.com>
2024-10-05 17:58:17 +0000

    "fix login"

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <body>
     <div class="login-container">
         <h2>Login</h2>
-        <form action="login.php" method="post">
+        <form action="user_login.php" method="post">
             <div class="form-group">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
--- user_login.php
+++ user_login.php
@@ -2,7 +2,7 @@
 session_start();
 include 'connection.php';
 if (isset($_SESSION['username'])) {
-    header('Location: maindashboard.php');
+    header('Location: main_dashboard.php');
     exit();
 }
 
