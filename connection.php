88a751fa696ab9d6ceaa517a85d0dae074e12d78
Nyx <61583597+mrlittleman@users.noreply.github.com>
2024-10-05 08:14:48 +0000

    added connection to database

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+    echo 'Connected to database';
+    }
+catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
+?>
